96a690fa05de7624558ec09324a519ab70c18fd4
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-17 18:25:43 -0500

    added models file, and cleaned up index.jsx by adding schemas

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-function connectDB() {
-    mongoose.connect(
-        config.get('mongoURI'),
-        { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log('Connected to MongoDB...'))
-        .catch((err)) => {
-            console.log('Could not connect to MongoDB. ERROR: ${err}');
-            process.exit(1);
-        }
-    );
-}
 
-module.exports = connectDB;
+mongoose
+.connect(
+    config.get('mongoURI'),
+    'mongodb+srv://<username>:<password>@<clustername>.6si6q.mongodb.net/
+<dbname>?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('Connected to MongoDB...'))
+ .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`));
+
